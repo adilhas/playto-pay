2075903ba7bcf330e1458f9870b6862d8c996e65
adilhas <hashmiadil161@gmail.com>
2026-04-26 16:48:30 +0530

    Phase 1 done initialized project structure and connected DB with JWT protected auth

---
+++ accounts/views.py
@@ -0,0 +1,7 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class MeView(APIView):
+    def get(self, request):
+        return Response({"user": request.user.username})
