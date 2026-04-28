5afb121b2262f7cf03bcf769b4c9c7d04d645782
adilhas <hashmiadil161@gmail.com>
2026-04-28 12:13:14 +0530

    added get req to view account details in accounts view

--- accounts/views.py
+++ accounts/views.py
@@ -12,6 +12,22 @@ class MeView(APIView):
 
 
 class BankAccountCreateView(APIView):
+    def get(self, request):
+        merchant = request.user.merchant
+
+        accounts = BankAccount.objects.filter(merchant=merchant)
+
+        data = [
+            {
+                "id": acc.id,
+                "account_number": acc.account_number,
+                "ifsc_code": acc.ifsc_code,
+            }
+            for acc in accounts
+        ]
+
+        return Response(data, status=status.HTTP_200_OK)
+
     def post(self, request):
         merchant = request.user.merchant
 
@@ -29,3 +45,23 @@ class BankAccountCreateView(APIView):
             },
             status=status.HTTP_201_CREATED,
         )
+
+
+# class BankAccountCreateView(APIView):
+#     def post(self, request):
+#         merchant = request.user.merchant
+
+#         bank = BankAccount.objects.create(
+#             merchant=merchant,
+#             account_number=request.data.get("account_number"),
+#             ifsc_code=request.data.get("ifsc_code"),
+#         )
+
+#         return Response(
+#             {
+#                 "id": bank.id,
+#                 "account_number": bank.account_number,
+#                 "ifsc_code": bank.ifsc_code,
+#             },
+#             status=status.HTTP_201_CREATED,
+#         )
