261ae82d8fc4af4ab69c5143f42719a8714907c9
adilhas <hashmiadil161@gmail.com>
2026-04-28 10:11:16 +0530

    updated added ledger list

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,5 +3,5 @@ from .views import BalanceView, LedgerListView
 
 urlpatterns = [
     path("balance/", BalanceView.as_view()),
-    path("entries/", LedgerListView.as_view()),
+    path("list/", LedgerListView.as_view()),
 ]
